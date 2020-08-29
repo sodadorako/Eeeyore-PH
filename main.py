32774844159a0d69ffe7e192b7ce1fc01edac6da
nattakits <40267160+nattakits@users.noreply.github.com>
2020-08-30 00:13:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,50 @@
 import requests
 from os import environ
 import time
+import tweepy
+from tweepy import OAuthHandler
+from tweepy import API
+import json
+import datetime as dt
+import  time
+
+access_token=environ['access_token']
+access_token_secret=environ['access_token_secret']
+consumer_key=environ['consumer_key']
+consumer_secret=environ['consumer_secret']
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = API(auth)
 
 
-url = 'https://notify-api.line.me/api/notify'
-token = environ['token']
-headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
+def trend_twitter():  #ดึงข้อมูล Trends Twitter
+    brazil_trends=api.trends_place(1118370)
+    trends = json.loads(json.dumps(brazil_trends, indent=1))
+    
+    Name_trend=[]
+    tweet_volume=[]
+    for i in trends[0]["trends"]:
+        if '#' in i["name"]:
+            Name_trend.append(i["name"]) 
+            tweet_volume.append(i["tweet_volume"])    
+           
+    return(Name_trend,tweet_volume)
+
+def top10(trend_text,A,B): #top n value 
+    trend_plot=[]
+
+    text='Top Trends Japan '+Time
+    for i in range(A,B):
+        text=text+'\n'+str(i+1)+') '+trend_text[i]
+        trend_plot.append(trend_text[i])
+    text=text
+    return(text)
+    
 
 while True:
-    msg='hello'
-    r = requests.post(url, headers=headers , data = {'message':msg})
-    time.sleep(300)
+    Timeupdate=dt.datetime.now()
+    Time=str(Timeupdate.strftime("%x"))+'  '+str(Timeupdate.strftime("%X"))
+    trend_text=trend_twitter()
+    text1=top10(trend_text[0],0,5)
+    api.update_status(status=text1)
+    time.sleep(1800)
