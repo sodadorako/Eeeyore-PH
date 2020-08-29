aa5c80ddd4287e276dc53c6f65e2fcd3dade5ba8
nattakits <40267160+nattakits@users.noreply.github.com>
2020-08-29 23:47:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
 from os import environ
+import time
+
+
 url = 'https://notify-api.line.me/api/notify'
 token = environ['token']
 headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
 
-msg='hello'
-
-r = requests.post(url, headers=headers , data = {'message':msg})
-
+while True:
+    msg='hello'
+    r = requests.post(url, headers=headers , data = {'message':msg})
+    time.sleep(300)
