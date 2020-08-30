d93ad44432b2ebd0b13822c0f941cb15b5dc113e
nattakits <40267160+nattakits@users.noreply.github.com>
2020-08-30 17:04:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def top10(trend_text,A,B): #top n value
 
 while True:
     Timeupdate=dt.datetime.now()
-    if(Timeupdate.minute==0 or Timeupdate.minute==30):
+    if(Timeupdate.minute==7 or Timeupdate.minute==37):
         Time=str(Timeupdate.strftime("%x"))+'  '+str(Timeupdate.strftime("%X"))
         trend_text=trend_twitter()
         text1=top10(trend_text[0],0,5)
