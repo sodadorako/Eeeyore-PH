ce4a053e601fbe5f19f4c16c977ce0758b7fb813
nattakits <40267160+nattakits@users.noreply.github.com>
2020-09-04 06:41:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,26 @@ import json
 from datetime import datetime,timedelta
 import pandas as pd
 import requests
+from io import BytesIO
 
 url = 'https://notify-api.line.me/api/notify'
 token = environ['token']
 headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
 
+r = requests.get(environ['ggsh'])
+data = r.content
+
+df_slot1=pd.read_excel(BytesIO(data),sheet_name='Slot1')
+df_slot2=pd.read_excel(BytesIO(data),sheet_name='Slot2')
+#df=df.set_index('Time')
+d_slot1=df_slot1.to_dict('split')
+d_slot2=df_slot2.to_dict('split')
+
+
 while True:
     Timeupdate=dt.datetime.now()
     if(Timeupdate.minute%3==0):
-        msg=Timeupdate
+        msg=d_slot2['data'][0][5]
         r = requests.post(url, headers=headers , data = {'message':msg})
 
         
