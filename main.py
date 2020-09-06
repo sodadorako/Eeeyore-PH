2fab79b7b11447c9364a042295599650486dc2e4
nattakits <40267160+nattakits@users.noreply.github.com>
2020-09-06 08:37:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ from datetime import datetime,timedelta
 import pandas as pd
 import requests
 
+url = 'https://notify-api.line.me/api/notify'
+token = environ['token']
+headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
+msg ='Program runnning111'
+r = requests.post(url, headers=headers , data = {'message':msg})
+
+time.sleep(10)
+
 file_name = 'https://docs.google.com/spreadsheet/ccc?key=19TWYLSwgC4cJe9mslepF1-et9RSP-C3VxQEtYxSS2yw&output=xlsx'
 df_slot2 = pd.read_excel(file_name,sheet_name='Slot2')
 df_slot1 = pd.read_excel(file_name,sheet_name='Slot1')
@@ -20,24 +28,6 @@ d_slot2=df_slot2.to_dict('split')
 url = 'https://notify-api.line.me/api/notify'
 token = environ['token']
 headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
-msg ='Program runnning'
+msg ='Program runnning222'
 r = requests.post(url, headers=headers , data = {'message':msg})
 
-
-while True:
-    Timeupdate=dt.datetime.now()
-    if(Timeupdate.minute==29 or Timeupdate.minute==10):
-        Time=str(Timeupdate.strftime("%x"))+'  '+str(Timeupdate.strftime("%X"))
-        if(Timeupdate.minute==29):
-            timecheck=1
-        elif(Timeupdate.minute==10):
-            timecheck=2
-        for i in d_slot1['data']:
-            if(i[0]==Timeupdate.hour and i[1]==timecheck):
-                Tweets_slot1=i[5]
-        for i in d_slot2['data']:
-            if(i[0]==Timeupdate.hour and i[1]==timecheck):
-                Tweets_slot2=i[5]
-        msg=Tweets_slot2
-        r = requests.post(url, headers=headers , data = {'message':msg})
-    time.sleep(40)
